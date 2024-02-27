83f40d8138a8eb4d1cecf830a00150554bbe3f67
정수현 <windseed71@naver.com>
2024-02-27 17:22:07 +0900

    init

---
+++ src/main/java/com/suhyun/WebGame/WebGameApplication.java
@@ -0,0 +1,15 @@
+package com.suhyun.WebGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGameApplication {
+
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WebGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/suhyun/WebGame/WebGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suhyun.WebGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
